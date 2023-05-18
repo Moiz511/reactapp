1ababc71189f380f12fa6990f1c4c9d5dac1124d
Moiz Sheikh <sheikhmoiz9@gmail.com>
2023-05-18 10:47:02 +0500

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
- import './App.css';
+import './App.css';
 import Body from './components/Body';
 import Navbar from './components/Navbar';
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light'); // where it is dark or light
+  
+  const togglemode = ()=> {
+    if(mode==='light')
+    { setMode('dark');
+  document.body.style.background='grey';}
+else{
+  setMode('light');
+  
+  document.body.style.background='white';
+}
+  };
   return (
     <>
- <Navbar title="blog" About="About Us"/>
- <div className="container">
- <Body heading="enter the text"/>
+ <Navbar title="Text Analyzer"  mode={mode} togglemode={togglemode} />
+ <div className="container my-3" >
+ <Body heading="Enter your text here: " mode={mode} />
  </div>
     </>
   );
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,36 +1,46 @@
 import React, {useState} from "react";
 
-export default function Body(props) {
+  export default function Body(props) {
 
-  const [text, setText] = useState("enter the text here");
-// for the upper letter
-  const handleonclick = () => {
-    let newtext = text.toUpperCase();
-    setText(newtext)
-  };
-  // for the lower letter
-  const handleonlo = () => {
-    let newtext = text.toLowerCase();
-    setText(newtext)
-  };
-  // for the onchange 
-  const handleonchange = (event) => {
-    setText(event.target.value); 
-  };
+    const [text, setText] = useState("enter the text here");
+    
+  // for the upper letter
+    const handleonclick = () => {
+      let newtext = text.toUpperCase();
+      setText(newtext)
+    };
+    // for the lower letter
+    const handleonlo = () => {
+      let newtext = text.toLowerCase();
+      setText(newtext)
+    };
+    // for the clear text
+    const handleonclear = () => {
+      let newtext = ' ';
+      setText(newtext)
+    };
+    // for the onchange 
+    const handleonchange = (event) => {
+      setText(event.target.value); 
+    };
 
-  return (
-    <>
-    <h2>{props.heading}</h2>
-    <div className="mb-3 my-4">
-      <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="5"></textarea>
-    </div>
-    <button className="btn btn-primary mx-2" onClick={handleonclick}>convert to Upercase</button>
-    <button className="btn btn-primary mx-0.5" onClick={handleonlo}>convert to lowercase</button>
-    <div className="container my-3">
-      <h2>summary</h2>
-      <p> {text.split(" ").length} words and {text.length} character</p>
-      
-    </div>
-    </>
-  );
-}
+    return (
+      <>
+      <div className="container my-2" style={{backgroundcolor:props.mode==='light'?'grey':'light'}}>
+      <h2 style={{color:props.mode==='dark'?'white':'black'}}>{props.heading}</h2>
+      <div className="mb-3 my-4">
+        <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="5"></textarea>
+      </div>
+      <button className="btn btn-primary mx-2" onClick={handleonclick}>convert to Upercase</button>
+      <button className="btn btn-primary mx-0.5" onClick={handleonlo}>convert to lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleonclear}>clear</button>
+
+      <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h2>summary</h2>
+        <p> {text.split(" ").length} words and {text.length} character</p>
+        
+      </div>
+      </div>
+      </>
+    );
+  }
