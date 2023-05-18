e840cff0a4e3ba70b231b0eb93f480166ff5fb39
Moiz Sheikh <sheikhmoiz9@gmail.com>
2023-05-18 10:51:55 +0500

    update beauty

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-
-import './App.css';
-import Body from './components/Body';
-import Navbar from './components/Navbar';
-import React, { useState } from 'react';
+import "./App.css";
+import Body from "./components/Body";
+import Navbar from "./components/Navbar";
+import React, { useState } from "react";
 
 function App() {
-  const [mode, setMode] = useState('light'); // where it is dark or light
-  
-  const togglemode = ()=> {
-    if(mode==='light')
-    { setMode('dark');
-  document.body.style.background='grey';}
-else{
-  setMode('light');
-  
-  document.body.style.background='white';
-}
+  const [mode, setMode] = useState("light"); // where it is dark or light
+
+  const togglemode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.background = "grey";
+    } else {
+      setMode("light");
+
+      document.body.style.background = "white";
+    }
   };
   return (
     <>
- <Navbar title="Text Analyzer"  mode={mode} togglemode={togglemode} />
- <div className="container my-3" >
- <Body heading="Enter your text here: " mode={mode} />
- </div>
+      <Navbar title="Text Analyzer" mode={mode} togglemode={togglemode} />
+      <div className="container my-3">
+        <Body heading="Enter your text here: " mode={mode} />
+      </div>
     </>
   );
-  
 }
 export default App;
